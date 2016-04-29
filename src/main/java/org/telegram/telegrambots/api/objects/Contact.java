b31e6f3e5e251681fce2d3cda6a90cb5938ddf1f
antonu17 <antonu17@gmail.com>
2016-04-29 16:31:37 +0600

    Remove setters

--- src/main/java/org/telegram/telegrambots/api/objects/Contact.java
+++ src/main/java/org/telegram/telegrambots/api/objects/Contact.java
@@ -51,32 +51,16 @@ public class Contact implements IBotApiObject {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public Integer getUserID() {
         return userID;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
     }
 
     @Override
