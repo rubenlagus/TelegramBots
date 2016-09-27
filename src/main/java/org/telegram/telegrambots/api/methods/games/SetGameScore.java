b8c36ec4fbb6270d414374375fde682a9d203aa4
Rubenlagus <rberlopez@gmail.com>
2016-09-28 00:31:34 +0200

    1. Renaming

--- src/main/java/org/telegram/telegrambots/api/methods/games/SetGameScore.java
+++ src/main/java/org/telegram/telegrambots/api/methods/games/SetGameScore.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of TelegramBots.
  *
- * Foobar is free software: you can redistribute it and/or modify
+ * TelegramBots is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * TelegramBots is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with TelegramBots.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.telegram.telegrambots.api.methods.games;
--- src/main/java/org/telegram/telegrambots/api/objects/games/Animation.java
+++ src/main/java/org/telegram/telegrambots/api/objects/games/Animation.java
@@ -6,13 +6,13 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * TelegramBots is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with TelegramBots.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.telegram.telegrambots.api.objects.games;
 
