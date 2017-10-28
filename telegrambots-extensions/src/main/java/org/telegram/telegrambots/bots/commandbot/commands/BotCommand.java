6e98746b311ccfffe55ae209197deef846b88d29
Vadim Goroshevsky <vadim.goroshevsky@gmail.com>
2017-10-28 16:52:09 +0300

    Implement DefaultBotCommand, which permit to process user message directly in "execute" method

--- telegrambots-extensions/src/main/java/org/telegram/telegrambots/bots/commandbot/commands/BotCommand.java
+++ telegrambots-extensions/src/main/java/org/telegram/telegrambots/bots/commandbot/commands/BotCommand.java
@@ -1,6 +1,7 @@
 package org.telegram.telegrambots.bots.commandbot.commands;
 
 import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.AbsSender;
 
@@ -9,13 +10,7 @@ import org.telegram.telegrambots.bots.AbsSender;
  *
  * @author Timo Schulz (Mit0x2)
  */
-public abstract class BotCommand {
-    public final static String COMMAND_INIT_CHARACTER = "/";
-    public static final String COMMAND_PARAMETER_SEPARATOR_REGEXP = "\\s+";
-    private final static int COMMAND_MAX_LENGTH = 32;
-
-    private final String commandIdentifier;
-    private final String description;
+public abstract class BotCommand extends DefaultBotCommand {
 
     /**
      * Construct a command
@@ -25,45 +20,20 @@ public abstract class BotCommand {
      * @param description       the description of this command
      */
     public BotCommand(String commandIdentifier, String description) {
-
-        if (commandIdentifier == null || commandIdentifier.isEmpty()) {
-            throw new IllegalArgumentException("commandIdentifier for command cannot be null or empty");
-        }
-
-        if (commandIdentifier.startsWith(COMMAND_INIT_CHARACTER)) {
-            commandIdentifier = commandIdentifier.substring(1);
-        }
-
-        if (commandIdentifier.length() + 1 > COMMAND_MAX_LENGTH) {
-            throw new IllegalArgumentException("commandIdentifier cannot be longer than " + COMMAND_MAX_LENGTH + " (including " + COMMAND_INIT_CHARACTER + ")");
-        }
-
-        this.commandIdentifier = commandIdentifier.toLowerCase();
-        this.description = description;
+        super(commandIdentifier, description);
     }
 
     /**
-     * Get the identifier of this command
+     * Overridden method from DefaultBotCommand, for backwards compatibility
      *
-     * @return the identifier
+     * @param absSender absSender to send messages over
+     * @param message   the message with command, for further processing
+     * @param arguments passed arguments
      */
-    public final String getCommandIdentifier() {
-        return commandIdentifier;
-    }
-
-    /**
-     * Get the description of this command
-     *
-     * @return the description as String
-     */
-    public final String getDescription() {
-        return description;
-    }
 
     @Override
-    public String toString() {
-        return "<b>" + COMMAND_INIT_CHARACTER + getCommandIdentifier() +
-                "</b>\n" + getDescription();
+    public void execute(AbsSender absSender, Message message, String[] arguments) {
+        execute(absSender, message.getFrom(), message.getChat(), arguments);
     }
 
     /**
