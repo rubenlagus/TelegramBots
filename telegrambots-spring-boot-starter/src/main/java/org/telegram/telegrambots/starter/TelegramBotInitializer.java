72a81805aba70c9ca2edfe866420e9ff96c926df
chase <chase@mailbox.org>
2018-12-19 22:09:04 +0100

    Add annotation to allow bot to get botSession
    A method annotated with @AfterBotRegistration will be invoked after
    the bot is registered.
    
    Optionally the method can have a parameter of
    type BotSession to get passed the BotSession the bot was created with.

--- telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotInitializer.java
+++ telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotInitializer.java
@@ -1,13 +1,20 @@
 package org.telegram.telegrambots.starter;
 
-import java.util.List;
-import java.util.Objects;
-
 import org.springframework.beans.factory.InitializingBean;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.meta.generics.WebhookBot;
+import org.telegram.telegrambots.meta.logging.BotLogger;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
 
 /**
  * Receives all beand which are #LongPollingBot and #WebhookBot and register them in #TelegramBotsApi.
@@ -33,7 +40,8 @@ public class TelegramBotInitializer implements InitializingBean {
 	public void afterPropertiesSet() throws Exception {
 		try {
             for (LongPollingBot bot : longPollingBots) {
-                telegramBotsApi.registerBot(bot);
+                BotSession session = telegramBotsApi.registerBot(bot);
+                handleAfterRegistrationHook(bot, session);
             }
             for (WebhookBot bot : webHookBots) {
                 telegramBotsApi.registerBot(bot);
@@ -42,4 +50,47 @@ public class TelegramBotInitializer implements InitializingBean {
             throw new RuntimeException(e);
         }		
 	}
+
+	private void handleAnnotatedMethod(Object bot, Method method, BotSession session) throws InvocationTargetException, IllegalAccessException {
+        if (method.getParameterCount() > 1) {
+            BotLogger.warn(this.getClass().getSimpleName(),
+                    format("Method %s of Type %s has too many parameters",
+                            method.getName(),
+                            method.getDeclaringClass().getCanonicalName()
+                    )
+            );
+            return;
+        }
+        if (method.getParameterCount() == 0) {
+            method.invoke(bot);
+            return;
+        }
+        if (method.getParameterTypes()[0].equals(BotSession.class)) {
+            method.invoke(bot, session);
+            return;
+        }
+        BotLogger.warn(this.getClass().getSimpleName(),
+                format("Method %s of Type %s has invalid parameter type",
+                        method.getName(),
+                        method.getDeclaringClass().getCanonicalName()
+                )
+        );
+    }
+
+    private void handleAfterRegistrationHook(Object bot, BotSession botSession) {
+        for (Method m : bot.getClass().getMethods()) {
+            Stream.of(m.getAnnotations()).forEach(annotation -> System.out.println(annotation.annotationType().getName()));
+            if (m.getAnnotation(AfterBotRegistration.class) != null) {
+                try {
+                    handleAnnotatedMethod(bot, m, botSession);
+                } catch (InvocationTargetException | IllegalAccessException e) {
+                    BotLogger.error(this.getClass().getSimpleName(),
+                            format("Couldn't invoke Method %s of Type %s",
+                                    m.getName(), m.getDeclaringClass().getCanonicalName()
+                            )
+                    );
+                }
+            }
+        }
+    }
 }
