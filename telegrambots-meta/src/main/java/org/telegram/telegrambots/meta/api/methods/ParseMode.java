2ca6d985fb6b670762eb830e7868a7de1665ad3b
rubenlagus <rberlopez@gmail.com>
2019-12-31 11:40:47 +0100

    Update 4.5

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/ParseMode.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/ParseMode.java
@@ -3,8 +3,7 @@ package org.telegram.telegrambots.meta.api.methods;
 /**
  * @author Ruben Bermudez
  * @version 1.0
- * @brief Type of parse modes for text in messages
- * @date 10 of April of 2016
+ * Type of parse modes for text in messages
  */
 public class ParseMode {
     public static final String MARKDOWN = "Markdown";
