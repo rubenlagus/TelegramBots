915a3dcd41e855a66f1a76ea008bd346fa7ba5b2
rubenlagus <rberlopez@gmail.com>
2019-12-31 11:40:34 +0100

    Update 4.5

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/ParseMode.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/ParseMode.java
@@ -8,5 +8,6 @@ package org.telegram.telegrambots.meta.api.methods;
  */
 public class ParseMode {
     public static final String MARKDOWN = "Markdown";
+    public static final String MARKDOWNV2 = "MarkdownV2";
     public static final String HTML = "html";
 }
--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/ChatPhoto.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/ChatPhoto.java
@@ -10,7 +10,9 @@ import org.telegram.telegrambots.meta.api.interfaces.BotApiObject;
  */
 public class ChatPhoto implements BotApiObject {
     private static final String SMALLFILEID_FIELD = "small_file_id";
+    private static final String SMALLFILEUNIQUEID_FIELD = "small_file_unique_id";
     private static final String BIGFILEID_FIELD = "big_file_id";
+    private static final String BIGFILEUNIQUEID_FIELD = "big_file_unique_id";
 
     /**
      * Unique file identifier of a small chat photo (160x160).
@@ -19,11 +21,23 @@ public class ChatPhoto implements BotApiObject {
     @JsonProperty(SMALLFILEID_FIELD)
     private String smallFileId;
     /**
+     * Unique identifier for a big chat photo (160x160), which is supposed to be the same over time and for different bots.
+     * Can't be used to download or reuse the file.
+     */
+    @JsonProperty(SMALLFILEUNIQUEID_FIELD)
+    private String smallFileUniqueId;
+    /**
      * Unique file identifier of a big chat photo (640x640).
      * This file_id can be used only for photo download and only for as long as the photo is not changed.
      */
     @JsonProperty(BIGFILEID_FIELD)
     private String bigFileId;
+    /**
+     * Unique identifier for a big chat photo (640x640), which is supposed to be the same over time and for different bots.
+     * Can't be used to download or reuse the file.
+     */
+    @JsonProperty(BIGFILEUNIQUEID_FIELD)
+    private String bigFileUniqueId;
 
     public ChatPhoto() {
         super();
@@ -37,11 +51,21 @@ public class ChatPhoto implements BotApiObject {
         return bigFileId;
     }
 
+    public String getSmallFileUniqueId() {
+        return smallFileUniqueId;
+    }
+
+    public String getBigFileUniqueId() {
+        return bigFileUniqueId;
+    }
+
     @Override
     public String toString() {
         return "ChatPhoto{" +
                 "smallFileId='" + smallFileId + '\'' +
+                ", smallFileUniqueId='" + smallFileUniqueId + '\'' +
                 ", bigFileId='" + bigFileId + '\'' +
+                ", bigFileUniqueId='" + bigFileUniqueId + '\'' +
                 '}';
     }
 }
