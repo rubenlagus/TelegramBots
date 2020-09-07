13f6b1f336a402d50f443127dbcb3113cc5afb28
Mouamle <mouamle.hasan@gmail.com>
2020-09-08 01:18:18 +0300

    used slf4j for logging instead of `exception.printStackTrace()`

--- telegrambots/src/main/java/org/telegram/telegrambots/updatesreceivers/DefaultExceptionMapper.java
+++ telegrambots/src/main/java/org/telegram/telegrambots/updatesreceivers/DefaultExceptionMapper.java
@@ -1,5 +1,8 @@
 package org.telegram.telegrambots.updatesreceivers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
@@ -10,11 +13,11 @@ import javax.ws.rs.ext.ExceptionMapper;
  * @version 1.0
  */
 public class DefaultExceptionMapper implements ExceptionMapper<Throwable> {
+    private static final Logger log = LoggerFactory.getLogger(DefaultExceptionMapper.class);
 
     @Override
     public Response toResponse(Throwable exception) {
-        exception.printStackTrace();
+        log.error("Exception caught: ", exception);
         return Response.serverError().build();
     }
-
 }
