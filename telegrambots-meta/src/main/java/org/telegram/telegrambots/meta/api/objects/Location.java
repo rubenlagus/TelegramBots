bea0b95fc7e3c7d1312998bc3af8d3660e3f81d4
rubenlagus <rberlopez@gmail.com>
2020-10-31 16:50:18 +0000

    Update Location fields to Double to avoid roundings

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/Location.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/Location.java
@@ -2,41 +2,26 @@ package org.telegram.telegrambots.meta.api.objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.telegram.telegrambots.meta.api.interfaces.BotApiObject;
 
 /**
  * @author Ruben Bermudez
  * @version 1.0
- * @brief This object represents a point on the map.
- * @date 20 of June of 2015
+ * This object represents a point on the map.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location implements BotApiObject {
 
     private static final String LONGITUDE_FIELD = "longitude";
     private static final String LATITUDE_FIELD = "latitude";
 
     @JsonProperty(LONGITUDE_FIELD)
-    private Float longitude; ///< Longitude as defined by sender
+    private Double longitude; ///< Longitude as defined by sender
     @JsonProperty(LATITUDE_FIELD)
-    private Float latitude; ///< Latitude as defined by sender
-
-    public Location() {
-        super();
-    }
-
-    public Float getLongitude() {
-        return longitude;
-    }
-
-    public Float getLatitude() {
-        return latitude;
-    }
-
-    @Override
-    public String toString() {
-        return "Location{" +
-                "longitude=" + longitude +
-                ", latitude=" + latitude +
-                '}';
-    }
+    private Double latitude; ///< Latitude as defined by sender
 }
