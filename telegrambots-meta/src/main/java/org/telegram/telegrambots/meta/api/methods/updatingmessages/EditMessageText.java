4af620770b399bb6156988f51765dd8aacf24d21
rubenlagus <rberlopez@gmail.com>
2020-11-08 11:35:09 +0000

    Remove old method messed

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/updatingmessages/EditMessageText.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/updatingmessages/EditMessageText.java
@@ -86,14 +86,6 @@ public class EditMessageText extends BotApiMethod<Serializable> {
     @JsonProperty(ENTITIES_FIELD)
     private List<MessageEntity> entities; ///< Optional. List of special entities that appear in message text, which can be specified instead of parse_mode
 
-    /**
-     * @deprecated Use {#setChatId(String)}
-     */
-    @Deprecated
-    public void setChatId(Long chatId) {
-        this.chatId = chatId.toString();
-    }
-
     public void disableWebPagePreview() {
         disableWebPagePreview = true;
     }
