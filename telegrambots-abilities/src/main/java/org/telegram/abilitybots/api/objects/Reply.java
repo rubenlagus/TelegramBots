edf21e5fe8ab6a937306b3efce76398a06cbd6eb
montpelllier <1565975426@qq.com>
2022-05-21 22:21:53 +0800

    Remove function isOkFor so the bot can take action normally on replies with document.

--- telegrambots-abilities/src/main/java/org/telegram/abilitybots/api/objects/Reply.java
+++ telegrambots-abilities/src/main/java/org/telegram/abilitybots/api/objects/Reply.java
@@ -52,24 +52,6 @@ public class Reply {
     return Reply.of(action, newArrayList(conditions));
   }
 
-  public static BiConsumer<BaseAbilityBot, Update> ToBiConsumer(Consumer<Update> consumer) {
-    return (baseAbilityBot, update) -> consumer.accept(update);
-  }
-
-  public static Reply of(Consumer<Update> action, List<Predicate<Update>> conditions) {
-    return new Reply(conditions, ToBiConsumer(action));
-  }
-
-  public static Reply of(Consumer<Update> action, Predicate<Update>... conditions) {
-    return Reply.of(ToBiConsumer(action), newArrayList(conditions));
-  }
-
-  public boolean isOkFor(Update update) {
-    // The following variable is required to avoid bug #JDK-8044546
-    BiFunction<Boolean, Predicate<Update>, Boolean> stateAnd = (state, cond) -> state && cond.test(update);
-    return conditions.stream().reduce(true, stateAnd, Boolean::logicalAnd);
-  }
-
   public void actOn(BaseAbilityBot bot, Update update) {
     action.accept(bot, update);
   }
