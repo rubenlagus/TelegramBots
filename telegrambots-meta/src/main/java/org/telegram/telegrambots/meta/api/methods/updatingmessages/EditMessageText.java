f2d092739de66bb0b3ec36bacb805b676dfd9e44
Dartanum <dartanum2@yandex.ru>
2023-08-18 01:55:55 +0300

    Add support markdown V2 to editMessageText method

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/updatingmessages/EditMessageText.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/methods/updatingmessages/EditMessageText.java
@@ -99,6 +99,14 @@ public class EditMessageText extends BotApiMethodSerializable {
         }
     }
 
+    public void enableMarkdownV2(boolean enable) {
+        if (enable) {
+            this.parseMode = ParseMode.MARKDOWNV2;
+        } else {
+            this.parseMode = null;
+        }
+    }
+
     public void enableHtml(boolean enable) {
         if (enable) {
             this.parseMode = ParseMode.HTML;
