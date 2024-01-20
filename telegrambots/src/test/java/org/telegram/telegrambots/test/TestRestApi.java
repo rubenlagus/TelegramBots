85f0b10ceb7ccdda4da826af986655a615d7449f
ruben <rberlopez@gmail.com>
2024-01-20 14:23:52 +0000

    API 7.0

--- telegrambots/src/test/java/org/telegram/telegrambots/test/TestRestApi.java
+++ telegrambots/src/test/java/org/telegram/telegrambots/test/TestRestApi.java
@@ -53,8 +53,9 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 /**
  * @author Ruben Bermudez
