bf60c09c67337b3917732b0682c483d729c1524a
Andrea Falco <andryxx_96@hotmail.it>
2024-03-01 13:49:03 +0100

    Update TelegramBotStarterConfiguration.java

--- telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotStarterConfiguration.java
+++ telegrambots-spring-boot-starter/src/main/java/org/telegram/telegrambots/starter/TelegramBotStarterConfiguration.java
@@ -1,10 +1,10 @@
 package org.telegram.telegrambots.starter;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
@@ -15,7 +15,7 @@ import java.util.List;
 /**
  * #TelegramBotsApi added to spring context as well
  */
-@Configuration
+@AutoConfiguration
 @ConditionalOnProperty(prefix = "telegrambots", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class TelegramBotStarterConfiguration {
 
