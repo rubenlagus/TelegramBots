1479cfdca201b473b57651e20e15cde3c9412431
Anjali Rachel Benjamin <an653262@dal.ca>
2024-04-01 21:13:28 -0300

    - Made Implementation Smell Refactoring to the class(1):
    Refactoring Name: Rename method/variable
    Package name: org.telegram.telegrambots.meta.api.methods.adminrights
    Class: SetMyDefaultAdministratorRightsTest
    Method: testSetMyDefaultAdministratorRightsWithNone()

--- telegrambots-meta/src/test/java/org/telegram/telegrambots/meta/api/methods/adminrights/SetMyDefaultAdministratorRightsTest.java
+++ telegrambots-meta/src/test/java/org/telegram/telegrambots/meta/api/methods/adminrights/SetMyDefaultAdministratorRightsTest.java
@@ -14,6 +14,7 @@ public class SetMyDefaultAdministratorRightsTest {
 
     @Test
     public void testDefaultRights() {
+
         SetMyDefaultAdministratorRights defaultRights = SetMyDefaultAdministratorRights
                 .builder()
                 .build();
