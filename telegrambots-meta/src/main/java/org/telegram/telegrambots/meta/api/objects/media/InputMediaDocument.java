ba943acb68ab5bc91845e783de519e9a08555195
Javier <jgg8340@gmail.com>
2025-03-14 23:57:59 +0100

    fix/javadoc/brackets (#1486)
    Fixed brackets on javadoc to show properly
    
    Co-authored-by: Ruben Bermudez <rubenlagus@users.noreply.github.com>

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/media/InputMediaDocument.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/media/InputMediaDocument.java
@@ -42,8 +42,8 @@ public class InputMediaDocument extends InputMedia {
      * Thumbnail of the file sent. The thumbnail should be in JPEG format and less than 200 kB in size.
      * A thumbnail’s width and height should not exceed 320.
      * Ignored if the file is not uploaded using multipart/form-data.
-     * Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>”
-     * if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+     * Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://&lt;file_attach_name&gt;”
+     * if the thumbnail was uploaded using multipart/form-data under &lt;file_attach_name&gt;.
      */
     private InputFile thumbnail;
     /**
